eeca4d0c9081b15d7bb38c97dfce8fd574ada795
Andrei Bernatski <iandy1731@gmail.com>
2022-08-03 16:53:44 +0300

    added redux store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,8 @@
 import { combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    
+});
 
 export const store = createStore(rootReducer, composeWithDevTools())
