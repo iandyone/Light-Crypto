9b35f89e3a962024e5e0c6d2bdf1737dec7103db
Andrei Bernatski <iandy1731@gmail.com>
2023-05-20 12:26:44 +0300

    fix dislayed currency median value

--- src/helpers/getFormattedData.jsx
+++ src/helpers/getFormattedData.jsx
@@ -9,7 +9,7 @@ export function getFormattedData(header, value) {
         case "Volume24h":
             return `$${new Intl.NumberFormat(["ban", "id"]).format(value.toFixed(2))}`;
         case "Median":
-            return (value > 1000) ? `$${new Intl.NumberFormat(["ban", "id"]).format(value.toFixed(2))}` : `$${value}`;
+            return (value > 1000) ? `$${new Intl.NumberFormat(["ban", "id"]).format(value.toFixed(2))}` : `$${value.toFixed(4)}`;
         default:
             return value;
     }
